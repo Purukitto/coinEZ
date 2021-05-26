264ac9780db66e0f0d69e7f00a091dc9bd0c0add
Purukitto <purukitto@gmail.com>
2021-05-26 21:52:44 +0530

    🐛 FIX: Bot Info

--- commands/utility/info.js
+++ commands/utility/info.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 
 module.exports = {
-    name: 'info',
+    name: 'botinfo',
     description: 'Check bot information!',
     aliases: ['bot'],
     async execute(message, args) {
