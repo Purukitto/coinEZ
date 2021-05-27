6cd220944e77e6ba9231fbccd9d16b9fa9d8b196
Purukitto <purukitto@gmail.com>
2021-05-27 14:02:25 +0530

    📦 NEW: Ping, Stats and Link Commands

---
+++ commands/utility/ping.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'ping',
+    aliases: ['pong', 'heart', 'heartbeat'],
+    description: 'Check the current latency of the bot',
+    async execute(bot, message) {
+        const m = await message.channel.send('Ping?');
+        m.edit(`🏓 Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. Heartbeat is ${bot.ws.ping}ms.`);
+    },
+};
---
+++ commands/utility/stats.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const ms = require('pretty-ms');
+const os = require('os');
+
+module.exports = {
+    name: 'stats',
+    aliases: ['botinfo', 'stat'],
+    description: 'Check the important information about the bot',
+    async execute(bot, message) {
+        const usedMemory = os.totalmem() - os.freemem(),
+            totalMemory = os.totalmem();
+        const getpercentage = ((usedMemory / totalMemory) * 100).toFixed(2) + '%';
+        const stats = new Discord.MessageEmbed()
+            .setTitle('🖥️ Bot Stats')
+            .addField('❯ Bot Latency', '``' + bot.ws.ping + '  ms``', true)
+            .addField('❯ Bot Uptime', '``' + ms(bot.uptime, {
+                verbose: true,
+                secondsDecimalDigits: 0,
+            }) + '``', true)
+            .addField('❯ Active Shards', '``' + bot.ws.totalShards + ' 🟢``', true)
+            .addField('❯ Servers', '``' + bot.guilds.cache.array().length + '``', true)
+            .addField('❯ Channels', '``' + bot.channels.cache.array().length + '``', true)
+            .addField('❯ Users', '``' + bot.users.cache.array().length + '``', true)
+            .addField('❯ RAM Usage', '``' + (usedMemory / Math.pow(1024, 2)).toFixed(2) + ' MB``', true)
+            .addField('❯ Processor Usage', '``' + getpercentage + '``', true)
+            .addField('❯ Operating System', '``' + os.platform() + os.arch() + '``', true);
+        message.channel.send(stats);
+    },
+};
