8b776d0b84e8b01701de1417d545120eabe67864
Purukitto <purukitto@gmail.com>
2021-06-02 14:24:48 +0530

    📦 NEW: bank

---
+++ commands/economy/bank.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const { getClient } = require("../../database");
+
+module.exports = {
+    name: 'bank',
+    aliases: ['savings'],
+    description: 'Check your bank balance!',
+    async execute(bot, message) {
+
+        const dbclient = await getClient();
+        const result = await dbclient.db().collection("userData").find({ "id": { $eq: message.author.id } }).toArray();
+
+        const balance = new Discord.MessageEmbed()
+            .setTitle(`🏦 ${message.author.username}'s Savings Account`)
+            .setColor('#FFD700')
+            .setFooter('Remember EZGold is just game currency!');
+
+        if (!result[0]) {
+            doc = { id: message.author.id, bal: 0, bank: 0 }
+            await dbclient.db().collection("userData").insertOne(doc, (err) => {
+                if (err) throw err
+            });
+            balance.setDescription(`You have not desposited any amount in the bank! \n You bank balance is <:ezgold:848597364322074625> \`0\``);
+        } else {
+            if (!result[0].depTime) {
+                deptime = 0
+            } else deptime = result[0].depTime;
+
+            let dtime = message.createdTimestamp - deptime;
+            dtime = dtime / 31556952000;
+            const bankbal = result[0].bank * (1 + ((7 * dtime) / 100));
+
+            balance.setDescription(`You have <:ezgold:848597364322074625> \`${bankbal}\` in your savings account! \`ezwithdraw [amount]\` to withdraw any amount!`);
+        }
+        message.channel.send(balance);
+    },
+};
