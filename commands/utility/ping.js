45534a10d38912984f4c2196264c4cfeebc07f58
Purukitto <purukitto@gmail.com>
2022-04-08 12:08:01 +0530

    :sparkles: Slash base

--- commands/utility/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    aliases: ['pong', 'heartbeat'],
-    description: 'Check the current latency of the bot!',
-    async execute(bot, message) {
-        const m = await message.channel.send('Ping?');
-        m.edit(`🏓 Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. Heartbeat is ${bot.ws.ping}ms.`);
-    },
-};
